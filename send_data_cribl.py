d5b54c35a26c9f1d45a2ebb3a98fb4185b6b3516
Ymbert Galloix <ymbert@Ymberts-MacBook-Pro.local>
2024-12-30 17:49:25 +0100

    new version of the script

--- send_data_cribl.py
+++ send_data_cribl.py
@@ -1,49 +1,34 @@
-import logging
-from splunk_data_sender import SplunkSender
+import pycurl
+import json
+from io import BytesIO
 
+headers = ['Accept: application/json']
+buffer = BytesIO()
 
-splunk_conf = {
-    'endpoint': 'default.main.confident-gilbert-b0v5dbx.cribl.cloud',
-    'port': '8088',
-    'token': '39444963-6f62-756a-7551-534c4d654177',
-    'index': 'main',
-    'channel': '79e6e39a-eb92-4de5-94a0-37844a3b20a3', # GUID
-    'api_version': '1.0',
-    # 'hostname': 'hostname',  # manually set a hostname parameter, defaults to socket.gethostname()
-    # 'source': 'source',  # manually set a source, defaults to the log record.pathname
-    # 'source_type': '_json',  # manually set a source_type, defaults to 'generic_single_line'
-    # 'allow_overrides': True,  # Whether to look for one of the Splunk built-in parameters(index, host, ecc)
-    'verify': False,  # turn SSL verification on or off, defaults to True
-    # 'timeout': 60,  # timeout for waiting on a 200 OK from Splunk server, defaults to 60s
-    # 'retry_count': 5,  # Number of retry attempts on a failed/erroring connection, defaults to 5
-    # 'retry_backoff': 2.0,  # Backoff factor, default options will retry for 1 min, defaults to 2.0
-    'enable_debug': True  # turn on debug mode; prints module activity to stdout, defaults to False
-}
+c = pycurl.Curl()
+c.setopt(c.URL, 'https://www.covid19dataportal.org/api/backend/viral-sequences/sequences?page=1&size=15&format=JSON')
+c.setopt(c.HTTPHEADER, headers)
+c.setopt(c.WRITEDATA, buffer)
 
-splunk = SplunkSender(**splunk_conf)
+c.perform()
 
-is_alive = splunk.get_health()
-logging.info(is_alive)
-if not is_alive:
-    raise Exception("HEC not alive")
+headers = ['Content-Type: application/json', 'Authorization: Splunk 39444963-6f62-756a-7551-534c4d654177']
+data = json.loads(buffer.getvalue().decode('utf-8'))["entries"]
 
-# The first payload is a quote to the italian theme song of Hello!Spank
-txt_record = "Hello! Splunk resta con me, Hello! Splunk non te ne andare, Caro Splunk! gioca con me, siamo amici io e te."
-json_record = { # this record will be parsed as normal text due to default "sourcetype" conf param
-        "source": "spacecraft Discovery 1",
-        "host": "HAL9000",
-        # "sourcetype": "_json",  # source type without underscore to allow the override of this built-in parameter
-        "index": "main",
-        "event": {"message": "I am afraid I can't do that Dave.", "severity": "ERROR"},
-        "rack": "42",
-        "os": "Linux, obvious",
-        "arch": "x64"
-    }
-payloads = [txt_record, json_record]
+c.setopt(c.URL, 'https://default.main.confident-gilbert-b0v5dbx.cribl.cloud:8088/services/collector')
+c.setopt(c.HTTPHEADER, headers)
 
-splunk_res = splunk.send_data(payloads)
-logging.info(splunk_res)
+for entry in data:
 
-ack_id = splunk_res.get('ackId')
-splunk_ack_res = splunk.send_acks(ack_id)
-logging.info(splunk_ack_res)
+    data_post = json.dumps({"event": entry, "sourcetype": "api"})
+
+    c.setopt(c.POSTFIELDS, data_post)
+    c.setopt(c.WRITEDATA, buffer)
+
+    c.perform()
+
+c.close()
+
+response = buffer.getvalue()
+
+print(response.decode('utf-8'))
